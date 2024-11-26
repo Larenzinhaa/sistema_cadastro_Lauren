73f7dfa82d0fc60c3ac84389d7b519bbf68772fc
Ana Lauren Dourado <analaurendourado@gmail.com>
2024-11-26 16:21:27 -0300

    conclusão

--- conexao.php
+++ conexao.php
@@ -1,24 +1,28 @@
 <?php
-$servername = "localhost";
-$usermane = "root";
+$servername = "localhost: 3309";
+$username = "root";
 $password = "";
 $dbname = "sistema";
 
-$conn = new mysqli($servername, $usermane, $password, $dbname);
+$conn = new mysqli ($servername, $username, $password, $dbname);
 
-if ($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed:" . $conn->connect_error);
 }
 
-// Adiciona a coluna 'imagem' à tabela 'produtos' se ela não existir
-$sql = "SHOW COLUMNS FROM produtos LIKE 'Imagem'";
+// adiciona a coluna 'imagem' à tabela 'produtos' se ela não existir
+$sql = "SHOW COLUMNS FROM produtos LIKE 'imagem'";
 $result = $conn->query($sql);
+if ($result->num_rows == 0) {
+    $sql = "ALTER TABLE produtos ADD COLUMN imagem VARCHAR(255)";
+    $conn->query($sql);
+}
 
-// Adiciona a coluna 'Imagem' à tabela 'fornecedores' se ela não existir
-$sql = "SHOW COLUMNS FROM produtos LIKE 'Imagem'";
+// adiciona a coluna 'imagem' à tabela 'produtos' se ela não existir
+$sql = "SHOW COLUMNS FROM produtos LIKE 'imagem'";
 $result = $conn->query($sql);
-if ($result->num_rows == 0){
-    $sql = "ALTER TABLE fornecedores ADD COLUMN imagem VARCHAR(255)";
+if ($result->num_rows == 0) {
+    $sql = "ALTER TABLE fornecedores ADD COLUMN imagem VARCHAR}(255)";
     $conn->query($sql);
 }
 ?>
